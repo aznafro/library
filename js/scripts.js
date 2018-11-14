10ef41346df8d21397c279bd7472241b81cefd77
aznafro <joekwon123@yahoo.com>
2018-11-14 18:01:08 +0800

    use firebase as our model. use dynamically created DOM elements for the books instead of injecting HTML text

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,140 @@
-// Book Object 
-function Book(id, title, author, pages, read, synopsis) {
-	this.id = id;
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.read = read;
-	this.synopsis = synopsis;
-}
 
-Book.prototype.toggleRead = function() {
-	if(this.read == "Read") {
-		this.read = "have not read";
-	}
-};
-
-let id = 0;
-let bookObjects = [
-	new Book(id++, "The Hobbit", "J.R.R. Tolkien", 259, "Read", "A hobbit goes on an adventure and comes back after finding out something terrible has happened."),
-	new Book(id++, "Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 799, "Not Read", "Harry Potter finds out he's a wizard and goes to Hogwarts! There he meets new friends, enemies and the ultimate evil tied to his past!"),
-	new Book(id++, "Fahrenheit 451", "Ray Bradbury", 198, "Read", "Citizen digs deep and finds out what the dystopian society is all about."),
-	new Book(id++, "Brave New World", "Aldus Huxley", 240, "Not Read", "Another dystopian society, or is it?"),
-	new Book(id++, "Animal House", "George Orwell", 190, "Read", "Not all is as it seems, as the weary farm animals seek to dismantle the tyranny of their farmers.")
-];
-
-/**
-	
-	EVENT LISTENERS
-
-**/
-let addButton = document.querySelector(".btn-add");
+// init elements
 let addContainer = document.querySelector(".add-container");
+let addTitle = addContainer.querySelector("[name=\"title\"]");
+let addAuthor = addContainer.querySelector("[name=\"author\"]");
+let addPages = addContainer.querySelector("[name=\"pages\"]");
+let addSynopsis = addContainer.querySelector("[name=\"synopsis\"]");
+let form = addContainer.querySelector(".form");
+
+let removeContainer = document.querySelector(".remove-container");
+let removeText = removeContainer.querySelector(".action__text");
+let removeConfirm = removeContainer.querySelector(".remove__confirm");
+let removeNo = removeContainer.querySelector(".remove__cancel");
+let currentBookEl = null;
+
+let updateContainer = document.querySelector(".update-container");
+let updateTitle = updateContainer.querySelector("[name=\"title\"]");
+let updateAuthor = updateContainer.querySelector("[name=\"author\"]");
+let updatePages = updateContainer.querySelector("[name=\"pages\"]");
+let updateRead = updateContainer.querySelector("[name=\"read\"]");
+let updateSynopsis = updateContainer.querySelector("[name=\"synopsis\"]");
+let updateConfirm = updateContainer.querySelector(".update__confirm");
+
 let booksContainer = document.querySelector(".books-container");
+let booksList = document.querySelector(".books .row");
+let addButton = document.querySelector(".btn-add");
 let header = document.querySelector(".header");
 let screen = document.querySelector(".screen");
+
+let database = firebase.database();
+
+/*********************************
+
+		BOOK ELEMENT OBJECT
+
+*********************************/
+function BookElement() {
+
+}
+
+// static variable used for the animation delay
+BookElement.delay = 0;
+
+BookElement.prototype.create = (bookId, book) => {
+	let bookEl = document.createElement("div");
+	let article = document.createElement("article");
+	let titleEl = document.createElement("h3");
+	let pagesEl = document.createElement("p");
+	let authorEl = document.createElement("p");
+	let extraInfoBar = document.createElement("div");
+	let readEl = document.createElement("p");
+	let menuBox = document.createElement("div");
+	let menuIcon = document.createElement("i");
+	let menuEl = document.createElement("div");
+	let list = document.createElement("ul");
+	let updateEl = document.createElement("li");
+	let updateLink = document.createElement("a");
+	let removeEl = document.createElement("li");
+	let removeLink = document.createElement("a");
+	let synopsisEl = document.createElement("p");
+
+	bookEl.classList.add("track-sm-1-2");
+	bookEl.classList.add("track-md-1-3");
+	bookEl.classList.add("track-lg-1-4");
+
+	let dataIdAtt = document.createAttribute("data-id");
+	dataIdAtt.value = bookId;
+	bookEl.setAttributeNode(dataIdAtt);
+
+	article.classList.add("book");
+	article.style.animationDelay = BookElement.delay + "s";
+	BookElement.delay += .1;
+
+	titleEl.classList.add("title");
+	titleEl.textContent = book.title;
+
+	pagesEl.classList.add("pages");
+	pagesEl.textContent = book.pages + " pgs";
+
+	authorEl.classList.add("author");
+	authorEl.textContent = book.author;
+
+	extraInfoBar.classList.add("extra-info-bar");
+
+	readEl.classList.add("read");
+	readEl.textContent = book.read;
+
+	menuBox.classList.add("menu-box");
+
+	menuIcon.classList.add("fas");
+	menuIcon.classList.add("fa-ellipsis-h");
+
+	menuEl.classList.add("menu");
+
+	updateEl.classList.add("nav-item");
+	updateLink.classList.add("nav-link");
+	updateLink.classList.add("update");
+	updateLink.textContent = "Update";
+
+	removeEl.classList.add("nav-item");
+	removeLink.classList.add("nav-link");
+	removeLink.classList.add("remove");
+	removeLink.textContent = "Remove";
+
+	synopsisEl.classList.add("synopsis");
+	synopsisEl.textContent = book.synopsis;
+
+	updateEl.appendChild(updateLink);
+	removeEl.appendChild(removeLink);
+	list.appendChild(updateEl);
+	list.appendChild(removeEl);
+	menuEl.appendChild(list);
+
+	menuBox.appendChild(menuIcon);
+	menuBox.appendChild(menuEl);
+
+	extraInfoBar.appendChild(readEl);
+	extraInfoBar.appendChild(menuBox);
+
+	article.appendChild(titleEl);
+	article.appendChild(pagesEl);
+	article.appendChild(authorEl);
+	article.appendChild(extraInfoBar);
+	article.appendChild(synopsisEl);
+
+	bookEl.appendChild(article);
+
+	this.bookEl = bookEl;
+
+	return bookEl;
+}
+
+/****************************************
+	
+		ALL OTHER EVENT LISTENERS
+
+****************************************/
 addButton.addEventListener("click", function() {
 	showContainer(addContainer);
 });
@@ -49,17 +151,14 @@ addContainer.querySelector(".x").addEventListener("click", function() {
 	hideContainer(addContainer);
 });
 
-let removeContainer = document.querySelector(".remove-container");
 removeContainer.querySelector(".x").addEventListener("click", function() {
 	hideContainer(removeContainer);
 });
 
-let updateContainer = document.querySelector(".update-container");
 updateContainer.querySelector(".x").addEventListener("click", function() {
 	hideContainer(updateContainer);
 });
 
-let removeNo = document.querySelector(".remove__cancel");
 removeNo.addEventListener("click", function() {
 	hideContainer(removeContainer);
 });
@@ -69,97 +168,49 @@ removeNo.addEventListener("click", function() {
 			ADD BOOK
 
 *****************************/
-let addTitle = addContainer.querySelector("[name=\"title\"]");
-let addAuthor = addContainer.querySelector("[name=\"author\"]");
-let addPages = addContainer.querySelector("[name=\"pages\"]");
-let addSynopsis = addContainer.querySelector("[name=\"synopsis\"]");
+
+// model listener for add
+let booksRef = database.ref("books/");
+booksRef.on('child_added', function(book) {
+	renderHelper(book.key, book.val());
+});
+
+function writeBook(title, author, pages, read, synopsis) {
+	let id = database.ref().push().key;
+
+ 	database.ref("books/" + id).set({
+	    title: title,
+	    author: author,
+	    pages: pages,
+	    read: read,
+	    synopsis: synopsis
+  	});
+}
 
 // prevent form from refreshing the page
-let form = addContainer.querySelector(".form");
 form.addEventListener("submit", function(e) {
 	e.preventDefault();
 
 	let addRead = addContainer.querySelector("[type=\"radio\"]:checked");
-	let newBook = new Book(id++, 
-							addTitle.value, 
-							addAuthor.value, 
-							addPages.value,
-							addRead.value,
-							addSynopsis.value);
 
+	// update model
+	writeBook(addTitle.value, addAuthor.value, addPages.value, addRead.value, addSynopsis.value);
+
+	// reset
 	addTitle.value = "";
 	addAuthor.value = "";
 	addPages.value = "";
 	document.querySelector(".add-container [value=\"Not Read\"]").checked = true;
 	addSynopsis.value = "";
 
-	bookObjects.push(newBook);
-	renderHelper(newBook, bookObjects.length - 1);
-	hookUpBook(document.querySelector("[data-id=\"" + (id-1) + "\"]"));
 	hideContainer(addContainer);
 });
 
 // Render Books into list
-let booksList = document.querySelector(".books .row");
-function render() {
-	bookObjects.forEach(function(book, index) {
-		renderHelper(book, index);
-	});
-}
-
-/**
-	
-	HELPER FUNCTIONS
-
-**/
-function renderHelper(book, index) {
-	let bookEl = document.createElement("div");
-	booksList.innerHTML += "<div class=\"track-sm-1-2 track-md-1-3 track-lg-1-4\" data-id=\"" + book.id + "\">" +
-								"<article class=\"book\">" +
-									"<h3 class=\"title\">" + book.title + "</h3>" +
-									"<p class=\"pages\">" + book.pages + " pgs</p>" +
-									"<p class=\"author\">" + book.author + "</p>" +
-									"<div class=\"extra-info-bar\">" +
-										"<p class=\"read\">" + book.read + "</p>" +
-										"<div class=\"menu-box\">" +
-											"<i class=\"fas fa-ellipsis-h\"></i>" + 
-											"<div class=\"menu\">" +
-												"<ul>" +
-													"<li class=\"nav-item\"><a class=\"nav-link update\">Update</a></li>" +
-													"<li class=\"nav-item\"><a class=\"nav-link remove\">Remove</a></li>" +
-												"</ul>" +
-											"</div>" + 
-										"</div>" +
-									"</div>" +
-									"<p class=\"synopsis\">" + book.synopsis + "</p>" +
-					            "</article>" +
-					        "</div>";
-}
-
-function showContainer(container) {
-	container.classList.remove("hidden");
-	blur();
-}
-
-function hideContainer(container) {
-	container.classList.add("hidden");
-	unblur();
-}
-
-function blur() {
-	screen.classList.remove("hidden");
-
-	booksContainer.classList.add("blur");
-	addButton.classList.add("blur");
-	header.classList.add("blur");
-}
-
-function unblur() {
-	screen.classList.add("hidden");
-
-	booksContainer.classList.remove("blur");
-	addButton.classList.remove("blur");
-	header.classList.remove("blur");
+function renderHelper(bookId, book) {
+	let bookEl = new BookElement().create(bookId, book);
+	attachMenuEventListeners(bookEl);
+	booksList.appendChild(bookEl);
 }
 
 /************************
@@ -167,18 +218,20 @@ function unblur() {
 		REMOVE BOOK
 
 ************************/
-let removeText = removeContainer.querySelector(".action__text");
-let removeConfirm = removeContainer.querySelector(".remove__confirm");
 
-let currentBookEl = null;
+// model listener for remove
+booksRef.on('child_removed', function(book) {
+	// update ui
+  	currentBookEl.remove();
+});
+
+function removeBook(id) {
+	database.ref("books/" + id).remove();
+}
+
 function remove() {
 	// update model
-	let index = bookObjects.find(({ id }) => id == currentBookEl.getAttribute("data-id"));
-	bookObjects.splice(index, 1);
-
-	// update view
-	currentBookEl.remove();
-
+	removeBook(currentBookEl.getAttribute("data-id"));
 	hideContainer(removeContainer);
 }
 removeConfirm.addEventListener("click", remove);
@@ -188,36 +241,42 @@ removeConfirm.addEventListener("click", remove);
 		UPDATE BOOK
 
 ***************************/
-let updateTitle = updateContainer.querySelector("[name=\"title\"]");
-let updateAuthor = updateContainer.querySelector("[name=\"author\"]");
-let updatePages = updateContainer.querySelector("[name=\"pages\"]");
-let updateRead = updateContainer.querySelector("[name=\"read\"]");
-let updateSynopsis = updateContainer.querySelector("[name=\"synopsis\"]");
-let updateConfirm = updateContainer.querySelector(".update__confirm");
 
-function update() {
-	let book = bookObjects.find(({ id }) => id == currentBookEl.getAttribute("data-id"));
-	
-	// update model
-	book.title = updateTitle.value;
-	book.author = updateAuthor.value;
-	book.pages = updatePages.value;
-	book.read = updateRead.value;
-	book.synopsis = updateSynopsis.textContent;
-
+// model listener for update
+booksRef.on('child_changed', function(book) {
 	// update view
 	currentBookEl.querySelector(".title").textContent = book.title;
 	currentBookEl.querySelector(".author").textContent = book.author;
 	currentBookEl.querySelector(".pages").textContent = book.pages + " pgs";
 	currentBookEl.querySelector(".read").textContent = book.read;
 	currentBookEl.querySelector(".synopsis").textContent = book.synopsis;
+});
 
-	hideContainer(updateContainer);
+function updateBook(id, title, author, pages, read, synopsis) {
+ 	database.ref("books/" + id).update({
+	    title: title,
+	    author: author,
+	    pages: pages,
+	    read: read,
+	    synopsis: synopsis
+  	});
 }
 
+function update() {
+	let id = currentBookEl.getAttribute("data-id");
+	
+	// update model, listener will update view
+	updateBook(id, updateTitle.value, updateAuthor.value, updatePages.value, updateRead.value, updateSynopsis.textContent);
+	hideContainer(updateContainer);
+}
 updateConfirm.addEventListener("click", update);
 
-function hookUpBook(bookEl) {
+/**************************
+	
+	HELPER FUNCTIONS
+
+***************************/
+function attachMenuEventListeners(bookEl) {
 	// toggle menu button
 	bookEl.querySelector(".fa-ellipsis-h").addEventListener("click", function(e) {
 		e.stopPropagation();
@@ -261,11 +320,28 @@ function hookUpBook(bookEl) {
 	});
 }
 
-// render books on page
-render();
+function showContainer(container) {
+	container.classList.remove("hidden");
+	blur();
+}
 
-// menu listener
-let bookElements = document.querySelectorAll("[data-id]");
-bookElements.forEach(function(bookEl) {
-	hookUpBook(bookEl);
-});
+function hideContainer(container) {
+	container.classList.add("hidden");
+	unblur();
+}
+
+function blur() {
+	screen.classList.remove("hidden");
+
+	booksContainer.classList.add("blur");
+	addButton.classList.add("blur");
+	header.classList.add("blur");
+}
+
+function unblur() {
+	screen.classList.add("hidden");
+
+	booksContainer.classList.remove("blur");
+	addButton.classList.remove("blur");
+	header.classList.remove("blur");
+}
